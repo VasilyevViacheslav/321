b857e0294ccd1076159c57987bcce38a25507fc6
VasilyevViacheslav <slavavas2132@mail.ru>
2022-06-03 16:40:00 +0500

    231

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Window1 win = new Window1();
+            win.Owner = this;
+            win.Show();
+            this.Hide();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public string velocity, angle, weight, splitting;
+        ParabolaFlight pf;
+        int i = 1;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Get_Velocity(object sender, TextChangedEventArgs e)
+        {
+            velocity = Velocity.Text;
+        }
+
+        private void Get_Angle(object sender, TextChangedEventArgs e)
+        {
+            angle = Angle.Text;
+        }
+
+        private void Get_Weight(object sender, TextChangedEventArgs e)
+        {
+            weight = Weight.Text;
+        }
+
+        private void Get_Splitting(object sender, TextChangedEventArgs e)
+        {
+            splitting = Splitting.Text;
+        }
+
+        private void ButtonStartMove_Click(object sender, RoutedEventArgs e)
+        {
+            pf = new ParabolaFlight(Convert.ToDouble(velocity), Convert.ToDouble(angle),
+               Convert.ToDouble(weight), Convert.ToDouble(splitting));
+            pf.calculation(Canvas.GetLeft(Bird), Canvas.GetTop(Bird));
+
+            DispatcherTimer tmr = new DispatcherTimer();
+            tmr.Interval = TimeSpan.FromMilliseconds(0.5);
+            tmr.Tick += new EventHandler(TimerOnTick);
+            tmr.Start();
+
+            if (i >= pf.Points.Count)
+                tmr.Stop();
+            i = 0;
+        }
+
+        private void ButtonReturnToStart_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.SetLeft(Bird, pf.Points[0].x);
+            Canvas.SetTop(Bird, pf.Points[0].y);
+        }
+
+        void TimerOnTick (object sender, EventArgs e)
+        {
+            if (i < pf.Points.Count)
+            {
+                Canvas.SetLeft(Bird, pf.Points[i].x);
+                Canvas.SetTop(Bird, pf.Points[i].y);
+            }
+            i++;
+        }
+    }
+}
